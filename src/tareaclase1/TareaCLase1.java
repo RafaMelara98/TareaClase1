77638254aad5aa038daf6d36295991ee463e5fc0
Rafael Melara <Rafael Melara@DESKTOP-ICRJ4VN>
2018-03-15 18:56:59 -0600

    new commit

---
+++ src/tareaclase1/TareaCLase1.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tareaclase1;
+
+/**
+ *
+ * @author Rafael Melara
+ */
+public class TareaCLase1 {
+    
+    public static void main(String[] args) {
+       System.out.println( "Datos Generales ");
+       System.out.println( "Nombre: Rafael Alejandro  ");
+       System.out.println( "Apellidos: Melara Ramirez   ");
+       System.out.println( "Edad: 20 ");
+       System.out.println( "Fecha de nacimiento: 04/01/1998 ");
+       System.out.println( "Lugar de nacimiento:San Salvador ");
+       System.out.println( "Numero de DUI: 05643502-5 ");
+       
+        
+        
+                
+    }
+    
+}
